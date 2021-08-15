0fea1b0eb18fd89d411b11435101780332952420
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2021-08-15 19:14:31 -0300

    IMC FORM add OOP

--- assets/js/imcForm.js
+++ assets/js/imcForm.js
@@ -1,36 +1,50 @@
+class BodyMeasures {
+  constructor(userName, date, height, weight) {
+    this.userName = userName;
+    this.date = date;
+    this.height = height;
+    this.weight = weight;
+    this.bodyMassIndex = this.calculateBodyMassIndex(this.weight, this.height);
+  }
+
+  calculateBodyMassIndex(weight, height) {
+    weight = parseFloat(weight);
+    height = parseFloat(height);
+    let bodyMassIndex = (weight / (height * height)).toFixed(2);
+    return bodyMassIndex;
+  }
+}
+
 function callback(event) {
   // Prevent reload page on submit
   event.preventDefault();
   let formFields = {
-    name: document.querySelector("#form-name"),
+    userName: document.querySelector("#form-name"),
     date: document.querySelector("#form-date"),
     weight: document.querySelector("#form-weight"),
     height: document.querySelector("#form-height"),
   };
 
-  console.log(formFields.weight, formFields.height);
+  let bodyMeasure = new BodyMeasures(
+    formFields.userName.value,
+    new Date(formFields.date.value),
+    formFields.height.value,
+    formFields.weight.value
+  );
 
   let tableBody = document.querySelector("#imc-tbody");
   let tr = document.createElement("tr");
 
-  for (const [key, value] of Object.entries(formFields)) {
-    console.log(key, value);
+  for (const [key, value] of Object.entries(bodyMeasure)) {
     let td = document.createElement("td");
-    td.textContent = value.value;
+    td.textContent = value;
     tr.appendChild(td);
   }
 
-  let td = document.createElement("td");
-  td.textContent = (
-    formFields.weight.value /
-    (formFields.height.value * formFields.height.value)
-  ).toFixed(2);
-  tr.appendChild(td);
-
   tableBody.appendChild(tr);
 
-  this.reset();
-  formFields.name.focus();
+  // this.reset();
+  formFields.userName.focus();
 }
 
 function updateImcTable() {
