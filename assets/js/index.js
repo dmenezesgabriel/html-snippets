08b5f987ab1c5fe3e94d297eb989a8e160aebeb2
Gabriel <dmenezes.gabriel@gmail.com>
2021-09-01 22:42:40 -0300

    Fix side nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 function openSideNav() {
   document.querySelector(".side-nav").style.width = "250px";
-  document.querySelector(".top-nav-links").style.marginLeft = "260px";
+  document.querySelector(".top-nav").style.marginLeft = "250px";
+  document.querySelector(".footer").style.marginLeft = "250px";
+  document.querySelector("#openSideNav").style.display = "none";
 }
 
 function closeSideNav() {
   document.querySelector(".side-nav").style.width = "0";
-  document.querySelector(".top-nav-links").style.marginLeft = "0";
+  document.querySelector(".top-nav").style.marginLeft = "0";
+  document.querySelector(".footer").style.marginLeft = "0";
+  document.querySelector("#openSideNav").style.display = "Block";
 }
